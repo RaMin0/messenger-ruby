3f2d5ed62e35d700a5efbe168b7b6bb05c46ebd2
Ramy Aboul Naga <ramy.naga@gmail.com>
2017-03-10 21:07:00 +0200

    Add default_action to bubble

--- lib/messenger/components/elements/bubble.rb
+++ lib/messenger/components/elements/bubble.rb
@@ -5,14 +5,15 @@ module Messenger
     class Bubble
       include Components::Element
 
-      attr_accessor :title, :item_url, :image_url, :buttons
+      attr_accessor :title, :item_url, :image_url, :default_action, :buttons
 
-      def initialize(title:, subtitle: nil, item_url: nil, image_url: nil, buttons: nil)
-        @title     = title
-        @subtitle  = subtitle
-        @image_url = image_url
-        @item_url  = item_url
-        @buttons   = build_buttons(buttons)
+      def initialize(title:, subtitle: nil, item_url: nil, image_url: nil, default_action: nil, buttons: nil)
+        @title           = title
+        @subtitle        = subtitle
+        @image_url       = image_url
+        @item_url        = item_url
+        @default_action  = default_action
+        @buttons         = build_buttons(buttons)
       end
 
       def build_buttons(buttons)
