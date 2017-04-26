1c2f8b4dc67706849778efb18e7bfbdeac3196fe
Ramy Aboul Naga <ramy.naga@gmail.com>
2017-04-26 11:43:42 +0200

    Allow web_url button to take extra options

--- lib/messenger/components/elements/button.rb
+++ lib/messenger/components/elements/button.rb
@@ -2,11 +2,13 @@ module Messenger
   module Elements
     class Button
       attr_accessor :type, :title, :value
+      attr_accessor :options
 
-      def initialize(type:, title:, value:)
-        @type  = type
-        @title = title
-        @value = value
+      def initialize(type:, title:, value:, **options)
+        @type    = type
+        @title   = title
+        @value   = value
+        @options = options
       end
 
       def build
@@ -14,7 +16,9 @@ module Messenger
       end
 
       def web_url
-        { url: @value }
+        { url: @value }.merge(options.slice(
+          :webview_height_ratio, :webview_share_button, :messenger_extensions, :fallback_url
+        ))
       end
 
       def postback
