caf9b0a2504491a56bcfd91aa76506f1a84f2474
Ramy Aboul Naga <ramy.naga@gmail.com>
2017-05-30 09:28:21 +0200

    Added element_share button type

--- lib/messenger/components/elements/button.rb
+++ lib/messenger/components/elements/button.rb
@@ -12,21 +12,29 @@ module Messenger
       end
 
       def build
-        { type: @type, title: @title }.merge! eval(@type.to_s)
+        eval(@type.to_s)
+      end
+
+      def default
+        { type: @type, title: @title }
       end
 
       def web_url
-        { url: @value }.merge(options.slice(
+        default.merge(url: @value).merge(options.slice(
           :webview_height_ratio, :webview_share_button, :messenger_extensions, :fallback_url
         ))
       end
 
       def postback
-        { payload: @value }
+        default.merge(payload: @value)
       end
 
       def phone_number
-        { payload: @value }
+        default.merge(payload: @value)
+      end
+
+      def element_share
+        default.slice(:type)
       end
     end
   end
